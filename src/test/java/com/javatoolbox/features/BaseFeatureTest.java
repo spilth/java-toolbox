822f5607a2425a2f1de174781bba1653a1cf4724
Brian Kelly <polymonic@gmail.com>
2015-12-23 16:20:32 -0500

    Ensure Feature tests run using the "test" Spring profile.

--- src/test/java/com/javatoolbox/features/BaseFeatureTest.java
+++ src/test/java/com/javatoolbox/features/BaseFeatureTest.java
@@ -6,11 +6,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ToolboxApplication.class)
 @WebIntegrationTest(randomPort = true)
+@ActiveProfiles("test")
 public abstract class BaseFeatureTest extends SeleniumTest {
     @Value("${local.server.port}")
     private int localServerPort;
